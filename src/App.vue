7f4ad925ea07212aa5215d1ae0661723c494b738
Sam Steimer <samsteimer@gmail.com>
2024-05-24 11:37:38 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-slate-400">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue'
+
+</script>
+
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="sticky top-0  bg-slate-400 shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <RouterLink :to="{name: 'home' }">
+                <div class="flex items-center gap-3 flex-1">
+                    <i class="fa-solid fa-football text-2xl"></i>
+                    <p>Flag Football Lineups</p>
+                </div>
+            </RouterLink>
+
+            <div class="flex gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-black duration-150 cursor-pointer"></i>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
